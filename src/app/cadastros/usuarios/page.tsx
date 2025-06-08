e51f99154df47d13c7f7336cef773b89bbb0c328
Eric Silva <eric.fernando.gs@gmail.com>
2025-06-07 23:14:54 -0300

    esboço usuários

---
+++ src/app/cadastros/usuarios/page.tsx
@@ -0,0 +1,20 @@
+
+export default function UsuariosPage() {
+
+
+
+
+
+
+
+
+
+
+
+    return (
+    <div>
+      <h1>Usuários</h1>
+      {/* Aqui você pode adicionar o conteúdo da página de usuários */}
+    </div>
+  );
+}
